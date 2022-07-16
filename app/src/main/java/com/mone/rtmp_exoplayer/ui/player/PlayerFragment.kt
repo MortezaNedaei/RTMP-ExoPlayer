cb424a97f5b54f3b114a354d7fe30119a978f641
Morteza Nedaei <nedaei1375@gmail.com>
2022-07-17 00:54:36 +0430

    refactor: Update ExoPlayer to the 2.18.0 version

--- app/src/main/java/com/mone/rtmp_exoplayer/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/mone/rtmp_exoplayer/ui/player/PlayerFragment.kt
@@ -6,22 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory
+import com.google.android.exoplayer2.ext.rtmp.RtmpDataSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.mone.rtmp_exoplayer.databinding.FragmentPlayerBinding
 
 class PlayerFragment : Fragment() {
 
     private lateinit var binding: FragmentPlayerBinding
-    private lateinit var viewModel: PlayerViewModel
-    private lateinit var player: SimpleExoPlayer
+    private val viewModel by viewModels<PlayerViewModel>()
+    private lateinit var exoPlayer: ExoPlayer
     private val url = "rtmp://192.168.1.52/LiveApp/test"
     // audio: rtmp://202.123.27.133:1935/bestfm/livestream
 
@@ -33,17 +33,12 @@ class PlayerFragment : Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PlayerViewModel::class.java)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initializePlayer()
         playVideo()
-        player.playWhenReady = true
-        player.prepare()
+        exoPlayer.playWhenReady = true
+        exoPlayer.prepare()
     }
 
     /**
@@ -59,25 +54,22 @@ class PlayerFragment : Fragment() {
         )
         trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSizeSd())
 
-        player = SimpleExoPlayer.Builder(requireContext())
+        exoPlayer = ExoPlayer.Builder(requireContext())
             .setTrackSelector(trackSelector)
             .setBandwidthMeter(DefaultBandwidthMeter.getSingletonInstance(requireContext()))
             .build()
 
-        binding.simplePlayer.player = player
-        binding.simplePlayer.setShowBuffering(PlayerView.SHOW_BUFFERING_ALWAYS)
-        binding.simplePlayer.setPlaybackPreparer {
-            player.prepare()
-        }
+        binding.styledPlayer.player = exoPlayer
+        binding.styledPlayer.setShowBuffering(StyledPlayerView.SHOW_BUFFERING_ALWAYS)
     }
 
     /**
      * Play RTMP
      */
     private fun playVideo() {
-        val videoSource = ProgressiveMediaSource.Factory(RtmpDataSourceFactory())
+        val videoSource = ProgressiveMediaSource.Factory(RtmpDataSource.Factory())
             .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
-        player.setMediaSource(videoSource)
+        exoPlayer.setMediaSource(videoSource)
     }
 
 
